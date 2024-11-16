db86e7ac0e4ffa3273e1cae7f859bb966556868d
shubham <>
2024-11-16 10:31:48 +0530

    added tags for json

--- internal/model/stores_visit.go
+++ internal/model/stores_visit.go
@@ -3,9 +3,9 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type VisitInfo struct {
-	StoreID    string   `bson:"store_id"`
-	VisitTime  string   `bson:"visit_time"`
-	ImageURLs  []string `bson:"image_urls"`
+	StoreID    string   `bson:"store_id" json:"store_id"`
+	VisitTime  string   `bson:"visit_time" json:"visit_time"`
+	ImageURLs  []string `bson:"image_urls" json:"image_url"`
 	ImageUUIDs []string `bson:"image_uuids"`
 	Perimeters []int64  `bson:"perimeters"`
 }
@@ -15,6 +15,6 @@ type StoresVisit struct {
 	Status        string             `bson:"status"`
 	Error         string             `bson:"error"`
 	FailedStoreID string             `bson:"failed_store_id"`
-	Count         int                `bson:"count"`
-	Visits        []VisitInfo        `bson:"visits"`
+	Count         int                `bson:"count" json:"count"`
+	Visits        []VisitInfo        `bson:"visits" json:"visits"`
 }
