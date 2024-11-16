1b513791f76b538344087629556d890d685ed61a
shubham <>
2024-11-16 22:34:55 +0530

    added option to log to file or stdout

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -21,6 +21,8 @@ var (
 
 const bufferSize = 1000
 
+const LogToFile = false
+
 // NewLogger initializes a new Logger
 func newLogger() *Logger {
 	l := &Logger{
@@ -33,23 +35,41 @@ func newLogger() *Logger {
 
 // startLogging listens for log messages and writes them to a file
 func (l *Logger) startLogging() {
-	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
 
-	for {
-		select {
-		case msg := <-l.logChannel:
-			_, err := file.WriteString(fmt.Sprintf("%s: %s\n", time.Now().Format(time.RFC3339), msg))
-			if err != nil {
-				log.Println("Error writing to log file:", err)
-			}
-		case <-l.done:
-			return
+	if LogToFile {
+		file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
 		}
+		defer file.Close()
+
+		for {
+			select {
+			case msg := <-l.logChannel:
+				_, err := file.WriteString(fmt.Sprintf("%s: %s\n", time.Now().Format(time.RFC3339), msg))
+				if err != nil {
+					log.Println("Error writing to log file:", err)
+				}
+			case <-l.done:
+				return
+			}
+		}
+	} else {
+		for {
+			select {
+			case msg := <-l.logChannel:
+				// Output log message to stdout
+				_, err := fmt.Printf("%s: %s\n", time.Now().Format(time.RFC3339), msg)
+				if err != nil {
+					log.Println("Error writing to stdout:", err)
+				}
+			case <-l.done:
+				return
+			}
+		}
+
 	}
+
 }
 
 // Log sends a log message to the log channel
